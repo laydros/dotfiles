29c93f77876399165719dde7bda6f31c05960277
Jason Hamilton <git@laydros.net>
2022-10-31 13:35:18 -0400

    cleanup after running shellcheck

--- bin/clean_meta.sh
+++ bin/clean_meta.sh
@@ -8,7 +8,7 @@ sed -i \
 -e 's/author: your name/author: jason/' \
 -e 's/company: your company (optional)/company:/' \
 -e 's/license: license (GPL-2.0-or-later, MIT, etc)/license: GPL-2.0-or-later/' \
-$file
+"$file"
 
 # sed -i 's/author: your name/author: jason/'
 # sed -i 's/company: your company (optional)/company:/'
--- bin/obsd-post.sh
+++ bin/obsd-post.sh
@@ -40,14 +40,14 @@ mkdir -p /var/syncthing
 BASE_PKGS="git curl wget yadm exa nnn htop detox ncdu rsync ripgrep bat " \
 BASE_PKGS="$BASE_PKGS delta"
 echo "add base packages: $BASE_PKGS"
-pkg_add $BASE_PKGS
+pkg_add "$BASE_PKGS"
 
 DESKTOP_PKGS="syncthing firefox meld mpv irssi pandoc cyrus-sasl--"
 DESKTOP_PKGS="$DESKTOP_PKGS isync notmuch msmtp neofetch aria2 sxiv "
 DESKTOP_PKGS="$DESKTOP_PKGS libqrencode zathura abook audacious cmus lynx nmap"
 DESKTOP_PKGS="$DESKTOP_PKGS password-store mutt--gpgme-sasl"
 echo "add desktop packages: $DESKTOP_PKGS"
-pkg_add $DESKTOP_PKGS
+pkg_add "$DESKTOP_PKGS"
         
 
 echo "add syncthing init for laydros"
