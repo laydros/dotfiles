0d052111bf7015f1811dc75e5467e5db91edd0d9
Jason Hamilton <git@laydros.net>
2025-03-17 17:17:02 -0400

    implement split for linux and mac specific brewfiles

--- bin/sync-vscode-extensions.sh
+++ bin/sync-vscode-extensions.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-# Define the path to your Brewfile
-BREWFILE="$HOME/.config/brew/Brewfile"
+# VS Code Extension Sync Script
+#
+# Usage:
+# - Installs missing VS Code extensions listed in the Brewfile.
+# - Removes unlisted VS Code extensions.
+#
+# Notes:
+# - Works cross-platform (macOS & Linux).
+# - Extracts `vscode` entries from Brewfile-core and the OS-specific Brewfile.
 
-# Extract VS Code extensions listed in the Brewfile
-brewfile_extensions=$(grep "^vscode " "$BREWFILE" | awk '{print $2}' | tr -d '"')
+BREWFILE_DIR=~/.config/brew
+
+# Determine the OS-specific Brewfile
+if [[ "$(uname)" == "Darwin" ]]; then
+    OS_BREWFILE="$BREWFILE_DIR/Brewfile-mac"
+elif [[ "$(uname)" == "Linux" ]]; then
+    OS_BREWFILE="$BREWFILE_DIR/Brewfile-linux"
+else
+    echo "Unsupported OS. Exiting."
+    exit 1
+fi
+
+# Extract VS Code extensions from the Brewfiles
+brewfile_extensions=$(grep "^vscode " "$BREWFILE_DIR/Brewfile-core" "$OS_BREWFILE" 2>/dev/null | awk '{print $2}' | tr -d '"')
 
 # Get currently installed VS Code extensions
 installed_extensions=$(code --list-extensions)
